c5c32aba1ef6c1d2736026b310f33cf583af3043
kurades <kotentuoi1122@gmail.com>
2023-06-20 14:28:31 +0700

    add util

---
+++ util.js
@@ -0,0 +1,14 @@
+'use strict'
+import crypto from 'crypto'
+
+let id = null
+
+export default { genId }
+
+const genId = () => {
+    if (!id) {
+        id = crypto.randomBytes(20)
+        Buffer.from('-AT0001-').copy(id, 0)
+    }
+    return id
+}
